5dd5bc84cb4af4d0341b1e4e4da2bcce2d3ec3a5
Stepanryazan <offish.sk@gmail.com>
2024-09-12 07:28:26 +0300

    correction

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DocumentSearchView, DocumentDeleteView
 
 urlpatterns = [
-    path('', DocumentSearchView.as_view(), name='document_search'),
-    path('', DocumentDeleteView.as_view(), name='document_delete'),
+    path('search/', DocumentSearchView.as_view(), name='document_search'),
+    path('delete/<int:id>/', DocumentDeleteView.as_view(), name='document_delete')
 ]
